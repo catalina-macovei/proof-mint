50a514c841853b2fc6f0bd07c7e289fccde7c42f
Catalina Macovei <catalinamacovei@Catalinas-MacBook-Pro-2.local>
2025-03-01 14:02:44 +0200

    up veramo

--- frontend/src/veramo/inMemoryKeyStore.js
+++ frontend/src/veramo/inMemoryKeyStore.js
@@ -1,22 +1,29 @@
 // src/veramo/inMemoryKeyStore.js
 import { AbstractKeyStore } from '@veramo/key-manager';
 
-export class InMemoryKeyStore extends AbstractKeyStore {
+export class InMemoryKeyStore {
   constructor() {
-    super();
     this.keys = new Map();
   }
 
   async importKey(key) {
     this.keys.set(key.kid, key);
+    return true;
+  }
+
+
+  async set(key) {
+    // Ensure the key object includes the privateKey
+    this.keys.set(key.kid, key);
     return key;
   }
 
-  async getKey(kid) {
+  async get(kid) {
     return this.keys.get(kid);
   }
 
-  async getKeys() {
+  async list() {
     return Array.from(this.keys.values());
   }
 }
+
